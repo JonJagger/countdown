d37bd41b96643f076ad200b001ebf294e0eb44cb
Jon Jagger <jon@jaggersoft.com>
2016-09-21 13:55:51 +0100

    delete dead comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 
 Countdown::Application.routes.draw do
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  #root :to => 'countdown#index'
-
-  get '/cplusplus'       => 'countdown#cplusplus'
+  get '/cplusplus' => 'countdown#cplusplus'
 
 end
